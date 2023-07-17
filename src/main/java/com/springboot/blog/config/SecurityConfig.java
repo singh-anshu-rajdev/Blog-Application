3a67dd81e4185a270e0dd0fa1a5d75d290fb4331
singh.anshu.rajdev <anshu0007singh@gmail.com>
2023-07-17 12:51:48 +0530

    working on SecurityConfig

---
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.springboot.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+//    @Bean
+//    SecurityFilterChain securityfilterchain(HttpSecurity http) throws Exception {
+//
+//        http.csrf(httpSecurityCsrfConfigurer -> ).disable().authorizeHttpRequests((authorize) ->authorize.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+//        return http.build();
+//    }
+}
--- src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
