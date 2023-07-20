467b7e3475e2d18d12a0a247972c73dceaa27431
singh.anshu.rajdev <anshu0007singh@gmail.com>
2023-07-20 16:31:38 +0530

    Jwt token has been added

--- src/main/java/com/springboot/blog/config/JwtTokenProvider.java
+++ src/main/java/com/springboot/blog/config/JwtTokenProvider.java
@@ -4,7 +4,6 @@ import com.springboot.blog.exception.BlogAPIException;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
